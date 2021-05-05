1bd1a9938c797e8e97500499714afccff20d7af0
composerben <56130322+composerben@users.noreply.github.com>
2021-05-05 13:00:36 -0700

    initial push to heroku

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const {
   db: { username, password, database, host },
-} = require('./index');
+} = require("./index");
 
 module.exports = {
   development: {
@@ -8,7 +8,12 @@ module.exports = {
     password,
     database,
     host,
-    dialect: 'postgres',
-    seederStorage: 'sequelize',
+    dialect: "postgres",
+    seederStorage: "sequelize",
+  },
+  production: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    seederStorage: "sequelize",
   },
 };

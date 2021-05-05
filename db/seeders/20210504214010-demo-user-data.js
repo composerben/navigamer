07fcfc10dab4b9fa72a4eb17d6060d0fadd6932e
Ryan <DaltonR121@gmail.com>
2021-05-05 10:50:34 -0400

    Made one last change to user-data

--- db/seeders/20210504214010-demo-user-data.js
+++ db/seeders/20210504214010-demo-user-data.js
@@ -1,22 +1,21 @@
 'use strict';
 const bcrypt = require('bcryptjs');
-const password = bcrypt.hashSync('password', 10);
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [
-      { username: 'demoUser', hashedPassword: password, email: 'demo@demo.com', createdAt: new Date(), updatedAt: new Date() },
-      { username: 'Shadept', hashedPassword: password, email: 'Shadept@demo.com', createdAt: new Date(), updatedAt: new Date() },
-      { username: 'RecklessCyborg', hashedPassword: password, email: 'RecklessCyborg@demo.com', createdAt: new Date(), updatedAt: new Date() },
-      { username: 'SnowWerewolf', hashedPassword: password, email: 'SnowWerewolf@demo.com', createdAt: new Date(), updatedAt: new Date() },
-      { username: 'IronSailor', hashedPassword: password, email: 'IronSailor@demo.com', createdAt: new Date(), updatedAt: new Date() },
-      { username: 'Aromaster', hashedPassword: password, email: 'Aromaster@demo.com', createdAt: new Date(), updatedAt: new Date() },
-      { username: 'ScienceTucan', hashedPassword: password, email: 'ScienceTucan@demo.com', createdAt: new Date(), updatedAt: new Date() },
-      { username: 'AwkwardFrogger', hashedPassword: password, email: 'AwkwardFrogger@demo.com', createdAt: new Date(), updatedAt: new Date() },
-      { username: 'BubbleKoala', hashedPassword: password, email: 'BubbleKoala@demo.com', createdAt: new Date(), updatedAt: new Date() },
-      { username: 'ShadyRaspberry', hashedPassword: password, email: 'ShadyRaspberry@demo.com', createdAt: new Date(), updatedAt: new Date() },
-      { username: 'LightTermite', hashedPassword: password, email: 'LightTermite@demo.com', createdAt: new Date(), updatedAt: new Date() },
-      { username: 'Snowmantis', hashedPassword: password, email: 'Snowmantis@demo.com', createdAt: new Date(), updatedAt: new Date() },
+      { username: 'demoUser', hashedPassword: bcrypt.hashSync('password', 10), email: 'demo@demo.com', createdAt: new Date(), updatedAt: new Date() },
+      { username: 'Shadept', hashedPassword: bcrypt.hashSync('password', 10), email: 'Shadept@demo.com', createdAt: new Date(), updatedAt: new Date() },
+      { username: 'RecklessCyborg', hashedPassword: bcrypt.hashSync('password', 10), email: 'RecklessCyborg@demo.com', createdAt: new Date(), updatedAt: new Date() },
+      { username: 'SnowWerewolf', hashedPassword: bcrypt.hashSync('password', 10), email: 'SnowWerewolf@demo.com', createdAt: new Date(), updatedAt: new Date() },
+      { username: 'IronSailor', hashedPassword: bcrypt.hashSync('password', 10), email: 'IronSailor@demo.com', createdAt: new Date(), updatedAt: new Date() },
+      { username: 'Aromaster', hashedPassword: bcrypt.hashSync('password', 10), email: 'Aromaster@demo.com', createdAt: new Date(), updatedAt: new Date() },
+      { username: 'ScienceTucan', hashedPassword: bcrypt.hashSync('password', 10), email: 'ScienceTucan@demo.com', createdAt: new Date(), updatedAt: new Date() },
+      { username: 'AwkwardFrogger', hashedPassword: bcrypt.hashSync('password', 10), email: 'AwkwardFrogger@demo.com', createdAt: new Date(), updatedAt: new Date() },
+      { username: 'BubbleKoala', hashedPassword: bcrypt.hashSync('password', 10), email: 'BubbleKoala@demo.com', createdAt: new Date(), updatedAt: new Date() },
+      { username: 'ShadyRaspberry', hashedPassword: bcrypt.hashSync('password', 10), email: 'ShadyRaspberry@demo.com', createdAt: new Date(), updatedAt: new Date() },
+      { username: 'LightTermite', hashedPassword: bcrypt.hashSync('password', 10), email: 'LightTermite@demo.com', createdAt: new Date(), updatedAt: new Date() },
+      { username: 'Snowmantis', hashedPassword: bcrypt.hashSync('password', 10), email: 'Snowmantis@demo.com', createdAt: new Date(), updatedAt: new Date() },
   ], {});
   },
 

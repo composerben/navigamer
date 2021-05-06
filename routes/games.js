eaa171bf7bfd11674e8259e43122c1060e3a4175
composerben <56130322+composerben@users.noreply.github.com>
2021-05-06 16:54:24 -0700

    start implementing reviews

--- routes/games.js
+++ routes/games.js
@@ -1,66 +1,121 @@
-const express = require('express');
+const express = require("express");
 const db = require("../db/models");
-const path = require('path');
-const { csrfProtection, asyncHandler, addGameValidators } = require("./utils");
+const path = require("path");
+const {
+  csrfProtection,
+  asyncHandler,
+  addGameValidators,
+  addReviewValidators,
+} = require("./utils");
+
 const { validationResult } = require("express-validator");
 const router = express.Router();
 
-router.use(express.static(path.join(__dirname, '../public')));
-router.use(express.static(path.join(__dirname, '../assets')));
+router.use(express.static(path.join(__dirname, "../public")));
+router.use(express.static(path.join(__dirname, "../assets")));
 
 /* GET users listing. */
-router.get('/', asyncHandler(async (req, res) => {
-  const games = await db.Game.findAll({ order: ['gameName'] });
-  res.render('games', {
-    title: 'All Games',
-    games,
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const games = await db.Game.findAll({ order: ["gameName"] });
+    res.render("games", {
+      title: "All Games",
+      games,
+    });
   })
-}));
+);
 
-router.get('/add-game', csrfProtection, asyncHandler(async (req, res) => {
-  const game = db.Game.build();
-  
-  res.render('add-game', {
-    title: 'Add Game',
-    game,
-    csrfToken: req.csrfToken()
-  })
-}));
+router.get(
+  "/add-game",
+  csrfProtection,
+  asyncHandler(async (req, res) => {
+    const game = db.Game.build();
 
-router.post('/add-game', csrfProtection, addGameValidators, asyncHandler(async (req, res) => {
-  const { gameName, releaseDate, developer, imgUrl } = req.body;
-
-  const game = db.Game.build({
-    gameName, releaseDate, developer, imgUrl
-  });
-
-  const validatorErrors = validationResult(req);
-  
-  if (validatorErrors.isEmpty()) {
-    await game.save();
-    res.redirect('/games')
-  } else {
-    const errors = validatorErrors.array().map((error) => error.msg);
     res.render("add-game", {
       title: "Add Game",
       game,
-      errors,
       csrfToken: req.csrfToken(),
     });
-  }
-}));
-
-router.get('/:id(\\d+)', asyncHandler(async (req, res) => {
-  const game = await db.Game.findByPk(req.params.id, {
-    include: db.Platform
-  });
-  const platforms = game.Platforms
-  
-  res.render('single-game', {
-    title: 'All Games',
-    game,
-    platforms
   })
-}));
+);
+
+router.post(
+  "/add-game",
+  csrfProtection,
+  addGameValidators,
+  asyncHandler(async (req, res) => {
+    const { gameName, releaseDate, developer, imgUrl } = req.body;
+
+    const game = db.Game.build({
+      gameName,
+      releaseDate,
+      developer,
+      imgUrl,
+    });
+
+    const validatorErrors = validationResult(req);
+
+    if (validatorErrors.isEmpty()) {
+      await game.save();
+      res.redirect("/games");
+    } else {
+      const errors = validatorErrors.array().map((error) => error.msg);
+      res.render("add-game", {
+        title: "Add Game",
+        game,
+        errors,
+        csrfToken: req.csrfToken(),
+      });
+    }
+  })
+);
+
+router.get(
+  "/:id(\\d+)",
+  asyncHandler(async (req, res) => {
+    const game = await db.Game.findByPk(req.params.id, {
+      include: db.Platform,
+    });
+    const platforms = game.Platforms;
+
+    res.render("single-game", {
+      title: "All Games",
+      game,
+      platforms,
+    });
+  })
+);
+
+router.post(
+  "/:id(\\d+)",
+  csrfProtection,
+  addReviewValidators,
+  asyncHandler(async (req, res) => {
+    const { gameId, userId, rating, review } = req.body;
+
+    const gameReview = db.Review.build({
+      gameId,
+      userId,
+      rating,
+      review
+    });
+
+    const validatorErrors = validationResult(req);
+
+    if (validatorErrors.isEmpty()) {
+      await gameReview.save();
+      return; //Ehhhhh?????
+    } else {
+      const errors = validatorErrors.array().map((error) => error.msg);
+      res.render("add-game", {
+        title: "Add Game",
+        game,
+        errors,
+        csrfToken: req.csrfToken(),
+      });
+    }
+  })
+);
 
 module.exports = router;
--- routes/reviews.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const path = require('path');
-
-router.use(express.static(path.join(__dirname, '../public')));
-router.use(express.static(path.join(__dirname, '../assets')));
-
-router.post('/games/:id')

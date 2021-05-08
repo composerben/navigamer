71a6ce224d6e1d0ff5ea762d010808c7f8450550
Ryan <DaltonR121@gmail.com>
2021-05-08 19:59:54 -0400

    Merging alllll the gameshelf work

--- db/seeders/20210506180116-gameshelf-data.js
+++ db/seeders/20210506180116-gameshelf-data.js
@@ -3,7 +3,8 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
       return queryInterface.bulkInsert('Gameshelves', [
-        { name: 'My Collection', userId: 1, createdAt: new Date(), updatedAt: new Date() }
+        { name: 'My Collection', userId: 1, createdAt: new Date(), updatedAt: new Date() },
+        { name: `Iron Sailor's Favorites`, userId: 5, createdAt: new Date(), updatedAt: new Date() }
       ], {});
   },
 
--- public/javascripts/gameshelf.js
+++ public/javascripts/gameshelf.js
@@ -1,16 +1,25 @@
 const addShelfBtn = document.querySelector('#addGameshelf__btn');
 const shelfName__input = document.querySelector('#newGameShelf__input')
 const shelfContainer = document.querySelector('.gameshelfCollection__container');
-
+let selectShelf = document.querySelectorAll('.gameshelf__div');
+const divUserId = document.querySelector('#gameshelf__container')
 // const userId = addReviewButton.getAttribute("data-id");
 
-// PULL INITIAL DATABASE STATE
-const fillShelf = async (inputValue) => {
-  const res = await fetch('/users/')
- 
+// SELECT A GAMESHELF
+function addShelfListeners() {
+  selectShelf = document.querySelectorAll('.gameshelf__div');
+  selectShelf.forEach(shelf => {
+    shelf.addEventListener('click', async (event) => {
+      const shelfId = event.target.getAttribute('data-id')
+      const userId = divUserId.getAttribute('data-id');
+
+      const getGames = await fetch(`/users/${userId}/gameshelves/${shelfId}`);
+      const res = await getGames.json()
+      console.log(res);
+    });
+  });
 }
 
-fillShelf()
 // Create a gameshelf
 const createShelf = async (inputValue) => {
   const res = await fetch('/users/', {
@@ -22,19 +31,30 @@ const createShelf = async (inputValue) => {
   })
 
   const data = await res.json();
-  console.log(data);
+
 
   const div = document.createElement('div');
-  const title = document.createElement('h2')
+  const title = document.createElement('h2');
   title.innerText = data.name;
   div.appendChild(title);
   div.classList.add('gameshelf__div')
-  // console.log(shelfContainer)
+  div.setAttribute('data-id', data.gameshelfId)
   shelfContainer.appendChild(div)
+  addShelfListeners();
 }
 
+addShelfListeners();
+
+// BUTTON TO ADD NEW SHELF
 addShelfBtn.addEventListener('click', (event) => {
   event.preventDefault();
-
   const data = createShelf(shelfName__input.value)
 })
+
+
+// PULL INITIAL DATABASE STATE
+const fillShelf = async (inputValue) => {
+  const res = await fetch('/users/')
+}
+
+fillShelf();
